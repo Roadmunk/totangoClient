58c3027056514c1127144af526411cc6b119afb5
Suthan Nanda <suthan@roadmunk.com>
2017-06-08 16:58:06 -0400

    Address Tomas' comments

--- totangoClient.js
+++ totangoClient.js
@@ -24,27 +24,20 @@ JS.class(TotangoClient, {
 		 * @param {String} [module]
 		 */
 		trackActivity : Promise.coroutine(function*(accountID, userID, activity, module) {
-			try {
-				if (typeof accountID !== 'string' ||
-					typeof userID !== 'string' ||
-					typeof activity !== 'string' ||
-					typeof module !== 'string') {
-					throw new Error('totangoClient.trackActivity: Invalid parameters');
-				}
-				else {
-					const params = {
-						sdr_s : this.serviceID,
-						sdr_o : accountID,
-						sdr_u : userID,
-						sdr_a : activity,
-						sdr_m : module,
-					};
-					yield this.sendSDR(params);
-				}
-			}
-			catch (error) {
-				throw error;
-			}
+			if (typeof accountID !== 'string' ||
+				typeof userID !== 'string' ||
+				typeof activity !== 'string' ||
+				typeof module !== 'string')
+				throw new Error('totangoClient.trackActivity: Invalid parameters');
+
+			const params = {
+				sdr_s : this.serviceID,
+				sdr_o : accountID,
+				sdr_u : userID,
+				sdr_a : activity,
+				sdr_m : module,
+			};
+			yield this.sendSDR(params);
 		}),
 
 		/**
@@ -54,22 +47,15 @@ JS.class(TotangoClient, {
 		 * @param {Object} [attributes]
 		 */
 		setUserAttributes : Promise.coroutine(function*(accountID, userID, attributes) {
-			try {
-				if (typeof accountID !== 'string' || typeof userID !== 'string') {
-					throw new Error('totangoClient.setUserAttributes: Invalid parameters');
-				}
-				else {
-					const initialParams = {};
-					if (attributes.name) {
-						initialParams['sdr_u.name'] = attributes.name;
-						delete attributes.name;
-					}
-					yield this.setAttributes('sdr_u.', initialParams, { accountID : accountID, userID : userID }, attributes);
-				}
+			if (typeof accountID !== 'string' || typeof userID !== 'string')
+				throw new Error('totangoClient.setUserAttributes: Invalid parameters');
+
+			const initialParams = {};
+			if (attributes.name) {
+				initialParams['sdr_u.name'] = attributes.name;
+				delete attributes.name;
 			}
-			catch (error) {
-				throw error;
-			}
+			yield this.setAttributes('sdr_u.', initialParams, { accountID : accountID, userID : userID }, attributes);
 		}),
 
 		/**
@@ -78,65 +64,45 @@ JS.class(TotangoClient, {
 		 * @param {Object} [attributes]
 		 */
 		setAccountAttributes : Promise.coroutine(function*(accountID, attributes) {
-			try {
-				if (typeof accountID !== 'string') {
-					throw new Error('totangoClient.setAccountAttributes: Invalid parameters');
-				}
-				else {
-					const initialParams = {};
-					if (attributes.name) {
-						initialParams['sdr_odn'] = attributes.name;
-						delete attributes.name;
-					}
-					yield this.setAttributes('sdr_o.', initialParams, { accountID : accountID }, attributes);
-				}
+			if (typeof accountID !== 'string')
+				throw new Error('totangoClient.setAccountAttributes: Invalid parameters');
+
+			const initialParams = {};
+			if (attributes.name) {
+				initialParams['sdr_odn'] = attributes.name;
+				delete attributes.name;
 			}
-			catch (error) {
-				throw error;
-			}
+			yield this.setAttributes('sdr_o.', initialParams, { accountID : accountID }, attributes);
 		}),
 
 		setAttributes : Promise.coroutine(function*(prefix, initialParams, identity, attributes) {
-			try {
-				if (typeof attributes !== 'object') {
-					throw new Error('totangoClient: Invalid attributes');
-				}
-				else {
+			if (typeof attributes !== 'object')
+				throw new Error('totangoClient: Invalid attributes');
 
-					let params = {
-						sdr_s : this.serviceID,
-						sdr_o : identity.accountID,
-					};
-					if (identity.userID)  params['sdr_u'] = identity.userID;
-					params = extend(params, initialParams);
+			let params = {
+				sdr_s : this.serviceID,
+				sdr_o : identity.accountID,
+			};
+			if (identity.userID)  params['sdr_u'] = identity.userID;
+			params = extend(params, initialParams);
 
-					for (const attr in attributes)
-						params[prefix + attr] = attributes[attr];
+			for (const attr in attributes)
+				params[prefix + attr] = attributes[attr];
 
-					yield this.sendSDR(params);
-				}
-			}
-			catch (error) {
-				throw error;
-			}
+			yield this.sendSDR(params);
 		}),
 
 		sendSDR : Promise.coroutine(function*(params) {
 			let response;
 
-			try {
-				response = yield axios({
-					method : 'GET',
-					url    : 'https://sdr.totango.com/pixel.gif',
-					params : params,
-				});
+			response = yield axios({
+				method : 'GET',
+				url    : 'https://sdr.totango.com/pixel.gif',
+				params : params,
+			});
 
-				if (response.status !== 200 && response.status !== 201)
-					throw new Error(`Invalid request, status code: ${response.status}`);
-			}
-			catch (error) {
-				throw error;
-			}
+			if (response.status !== 200 && response.status !== 201)
+				throw new Error(`Invalid request, status code: ${response.status}`);
 		}),
 	},
 });
